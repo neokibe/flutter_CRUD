befc0fb00b4072b62d37e3e5bccfce13f1581759
Neo <ndivhuwo.nthambeleni@standardbank.co.za>
2020-04-06 13:25:33 +0200

    search functionality implemented

--- flutter_crud/lib/presentation/pages/home.dart
+++ flutter_crud/lib/presentation/pages/home.dart
@@ -4,69 +4,107 @@ import 'package:flutter_firestore_crud/data/model/members.dart';
 import 'package:flutter_firestore_crud/presentation/pages/add_member.dart';
 import 'package:flutter_firestore_crud/presentation/pages/member_details.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
   @override
-  Widget build(BuildContext context){
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController _searchQueryController = TextEditingController();
+  List filteredNames = new List();
+  bool _isSearching = false;
+  String searchQuery = "Search query";
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Family Members List'),
+        leading: _isSearching ? const BackButton() : Container(),
+        title: _isSearching ? _buildSearchField() : _buildSearchField(),
+        actions: _buildActions(),
+//        title: Text('Family Members List'),
+//        actions: <Widget>[IconButton(icon: Icon(Icons.search), onPressed: (){})],
       ),
       body: StreamBuilder(
-        stream: FirestoreService().getNotes() ,
-        builder: (BuildContext context, AsyncSnapshot<List<Note>> snapshot){
-          if(snapshot.hasError || !snapshot.hasData)
+        stream: FirestoreService().getNotes(),
+        builder: (BuildContext context, AsyncSnapshot<List<Note>> snapshot) {
+          if (snapshot.hasError || !snapshot.hasData)
             return CircularProgressIndicator();
+
+          new TextField(
+            decoration: InputDecoration(contentPadding: EdgeInsets.all(15.0)),
+          );
+
           return ListView.builder(
             itemCount: snapshot.data.length,
             itemBuilder: (BuildContext context, int index) {
               Note note = snapshot.data[index];
-              return ListTile(
-                title: Text(note.firstName ?? ""),
-                subtitle: Text(note.gender ?? ""),
 
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    IconButton(
-                      color: Colors.blue,
-                      icon: Icon(Icons.edit),
-                      onPressed: () => Navigator.push(context, MaterialPageRoute(
-                        builder: (_) => AddNotePage(note: note),
-                      )),
+              return Card(
+                child: ListTile(
+                  // isThreeLine: true,
+                  leading: Icon(Icons.account_circle),
+                  title: Text(note.firstName ?? ""),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(note.lastName ?? ""),
+                      Text(note.gender ?? "")
+                    ],
+                  ),
+
+                  //subtitle: Text(note.gender ?? ""),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      IconButton(
+                        color: Colors.blue,
+                        icon: Icon(Icons.edit),
+                        onPressed: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => AddNotePage(note: note),
+                            )),
+                      ),
+                      IconButton(
+                        color: Colors.red,
+                        icon: Icon(Icons.delete),
+                        onPressed: () => _deleteNote(context, note.id),
+                      ),
+                    ],
+                  ),
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => NoteDetailsPage(
+                        note: note,
+                      ),
                     ),
-                    IconButton(
-                      color: Colors.red,
-                      icon: Icon(Icons.delete),
-                      onPressed: () => _deleteNote(context, note.id),
-                    ),
-                  ],
-                ),
-                onTap: ()=>Navigator.push(
-                  context, MaterialPageRoute(
-                    builder: (_) => NoteDetailsPage(note: note,),
                   ),
                 ),
               );
-           },
+            },
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: (){
-          Navigator.push(context, MaterialPageRoute(
-            builder: (_) => AddNotePage()
-          ));
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (_) => AddNotePage()));
         },
       ),
     );
   }
 
-  void _deleteNote(BuildContext context,String id) async {
-    if(await _showConfirmationDialog(context)) {
+  void _deleteNote(BuildContext context, String id) async {
+    if (await _showConfirmationDialog(context)) {
       try {
         await FirestoreService().deleteNote(id);
-      }catch(e) {
+      } catch (e) {
         print(e);
       }
     }
@@ -74,23 +112,91 @@ class HomePage extends StatelessWidget {
 
   Future<bool> _showConfirmationDialog(BuildContext context) async {
     return showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (context) => AlertDialog(
-        content: Text("Are you sure you want to delete?"),
-        actions: <Widget>[
-          FlatButton(
-            textColor: Colors.red,
-            child: Text("Delete"),
-            onPressed: () => Navigator.pop(context,true),
-          ),
-          FlatButton(
-            textColor: Colors.black,
-            child: Text("No"),
-            onPressed: () => Navigator.pop(context,false),
-          ),
-        ],
-      )
+        context: context,
+        barrierDismissible: true,
+        builder: (context) => AlertDialog(
+          content: Text("Are you sure you want to delete?"),
+          actions: <Widget>[
+            FlatButton(
+              textColor: Colors.red,
+              child: Text("Delete"),
+              onPressed: () => Navigator.pop(context, true),
+            ),
+            FlatButton(
+              textColor: Colors.black,
+              child: Text("No"),
+              onPressed: () => Navigator.pop(context, false),
+            ),
+          ],
+        ));
+  }
+
+  Widget _buildSearchField() {
+    return TextField(
+      controller: _searchQueryController,
+      autofocus: true,
+      decoration: InputDecoration(
+        hintText: "Search Data...",
+        border: InputBorder.none,
+        hintStyle: TextStyle(color: Colors.white30),
+      ),
+      style: TextStyle(color: Colors.white, fontSize: 16.0),
+      onChanged: (query) => updateSearchQuery,
     );
+  }
+
+  List<Widget> _buildActions() {
+    if (_isSearching) {
+      return <Widget>[
+        IconButton(
+          icon: const Icon(Icons.clear),
+          onPressed: () {
+            if (_searchQueryController == null ||
+                _searchQueryController.text.isEmpty) {
+              Navigator.pop(context);
+              return;
+            }
+            _clearSearchQuery();
+          },
+        ),
+      ];
+    }
+
+    return <Widget>[
+      IconButton(
+        icon: const Icon(Icons.search),
+        onPressed: _startSearch,
+      ),
+    ];
+  }
+
+  void _startSearch() {
+    ModalRoute.of(context)
+        .addLocalHistoryEntry(LocalHistoryEntry(onRemove: _stopSearching));
+
+    setState(() {
+      _isSearching = true;
+    });
+  }
+
+  void updateSearchQuery(String newQuery) {
+    setState(() {
+      searchQuery = newQuery;
+    });
+  }
+
+  void _stopSearching() {
+    _clearSearchQuery();
+
+    setState(() {
+      _isSearching = false;
+    });
+  }
+
+  void _clearSearchQuery() {
+    setState(() {
+      _searchQueryController.clear();
+      updateSearchQuery("");
+    });
   }
 }
